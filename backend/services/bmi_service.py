b69d7ffb8b9b58a3141008707125343940544b91
alharuty <alla-pdg@hotmail.es>
2025-05-24 19:28:36 +0200

    fix: fixed BMI readOnly=True

--- backend/services/bmi_service.py
+++ backend/services/bmi_service.py
@@ -3,7 +3,7 @@ from core.logging_config import setup_logger
 logger = setup_logger(__name__)
 
 def calculate_bmi(weight: float, height: float) -> float:
-    logger.info(f"Calculando BMI para peso={weight}, altura={height}")
+    #logger.info(f"Calculando BMI para peso={weight}, altura={height}")
     if height <= 0:
         logger.error("Altura menor o igual a cero")
         raise ValueError("La altura debe ser mayor que cero.")
